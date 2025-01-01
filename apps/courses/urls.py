2759cfdd27410b781a857932d804f24128192964
Mr-Adwok <adwokbrave@gmail.com>
2025-01-01 13:49:49 +0200

    added some api

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('')
+     path('create/',views.create_view),
+    path('list/',views.list_view),
+    path('delete/<int:pk>/',views.delete_view),
+    path('update/<int:pk>/',views.update_view),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from .serializers import CourseSerializer
+from .models import Course
+from rest_framework import generics,status
+from rest_framework.response import Response
 
-# Create your views here.
+
+# a create class that creates Courses
+class CreateAPIView(generics.CreateAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+create_view = CreateAPIView.as_view()
+
+class ListAPIView(generics.ListAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+list_view = ListAPIView.as_view()
+
+class DeleteAPIView(generics.DestroyAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+    lookup_field = "pk"
+
+    def get_object(self):
+        # Use the primary key (pk) to retrieve the object
+        obj = super().get_object()
+
+        
+        return obj
+        return redirect('Course/list/')
+        
+        
+        
+
+
+delete_view = DeleteAPIView.as_view()
+
+
+
+class UpdateAPIView(generics.UpdateAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+    lookup_field = "pk"
+
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    # def put(self, request, *args, **kwargs):
+        
+    #     response = self.update(request, *args, **kwargs)
+    #     # if response.status_code == status.HTTP_200_OK:
+    #     #     return self.redirect_view()
+        
+    #     return response
+        
+    # def redirect_view(self):
+    #     return redirect('/Course/list/')
+        
+        
+update_view = UpdateAPIView.as_view()
+
+
