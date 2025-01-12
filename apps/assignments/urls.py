fe4c4b4df4bf45288cd85874cb25707d8b615ae0
Mr-Adwok <adwokbrave@gmail.com>
2025-01-13 01:59:41 +0200

    modified a url

--- apps/assignments/urls.py
+++ apps/assignments/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.create_List_view ),
-    path('detail/<int:pk>/',views.detail_view),
+    path('<int:pk>/',views.detail_view),
     path('delete/<int:pk>/',views.delete_view),
     path('update/<int:pk>/',views.update_view),
 ]
